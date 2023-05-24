7136a78a932a6ca4709b6f1aefff50d67b537eb6
Gyanendra Mishra <anomaly.the@gmail.com>
2023-05-24 18:35:30 +0100

    build with builder

---
+++ static_files/builder.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+NODE_ID=${ID:-0}
+CLUSTER_SIZE=${CLUSTER_SIZE:-1}
+
+
+# Clean up and env set up
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export BUILD_PATH=/sei-protocol/sei-chain/build
+export PATH=$GOBIN:$PATH:/usr/local/go/bin:$BUILD_PATH
+echo "export GOPATH=$HOME/go" >> /root/.bashrc
+echo "GOBIN=$GOPATH/bin" >> /root/.bashrc
+echo "export PATH=$GOBIN:$PATH:/usr/local/go/bin:$BUILD_PATH" >> /root/.bashrc
+/bin/bash -c "source /root/.bashrc"
+
+/usr/bin/build.sh
