445e04a84cd2bdfeddbd1c880a5d91dbd2188e9c
Gyanendra Mishra <anomaly.the@gmail.com>
2023-05-25 09:44:22 +0100

    lets go

---
+++ static_files/step_4_and_5.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+NODE_ID=${ID:-0}
+CLUSTER_SIZE=${CLUSTER_SIZE:-1}
+
+export ID=${ID:-0}
+export CLUSTER_SIZE=${CLUSTER_SIZE:-4}
+
+# Clean up and env set up
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export BUILD_PATH=/sei-protocol/sei-chain/build
+export PATH=$GOBIN:$PATH:/usr/local/go/bin:$BUILD_PATH
+echo "export GOPATH=$HOME/go" >> /root/.bashrc
+echo "GOBIN=$GOPATH/bin" >> /root/.bashrc
+echo "export PATH=$GOBIN:$PATH:/usr/local/go/bin:$BUILD_PATH" >> /root/.bashrc
+/bin/bash -c "source /root/.bashrc"
+
+# Step 4: Configure persistent peers
+/usr/bin/persistent_peers.sh
+
+# Step 5: Start the chain
+/usr/bin/start_sei.sh
